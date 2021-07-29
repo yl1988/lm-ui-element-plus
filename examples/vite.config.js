6629369b90d1be781e415e498f6cc866d5fe67b6
杨涟 <yanglian@7build.com>
2021-07-29 14:45:27 +0800

    组件测试

--- examples/vite.config.js
+++ examples/vite.config.js
@@ -2,7 +2,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
-
+let proxys=function (){
+  let apiPathArr=['/code','/auth','/admin','/file-management','/park-management','/oa']
+  let proxys={}
+  for(let i=0;i<apiPathArr.length;i++){
+    proxys[apiPathArr[i]]={
+      target: 'http://10.2.100.23',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(new RegExp('/'+apiPathArr[i]), '')
+    }
+  }
+  return proxys
+}()
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,6 +25,8 @@ export default defineConfig({
     })
   ],
   server:{
-    host:'0.0.0.0'
+    host:'0.0.0.0',
+    port:'8085',
+    proxy:proxys
   }
 })
